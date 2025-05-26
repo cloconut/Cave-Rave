3c66501c6d20d22c2defd88858acc5774bce105b
cloconut <campbellc@macstudent.com.au>
2025-05-26 01:41:09 +0000

    getters and setters

--- cave.py
+++ cave.py
@@ -2,3 +2,19 @@ class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
         self.description = None
+        self.linked_caves = {}
+        
+    def get_description(self):
+        return self.description
+
+    def set_description(self, cave_description):
+        self.description = cave_description
+
+    def describe(self):
+        print(self.description)
+
+    def get_name(self, cave_name):
+        print(cave_name)
+
+    def set_name(self, cave_name):
+        cave_name = input('What will you name this cave?')
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from cave import Cave
 
 cavern = Cave('Cavern')
+grotto = Cave('Grotto')
+dungeon = Cave('Dungeon')
+
+cavern.set_description('A damp and dirty cave.')
+
+cavern.describe()
