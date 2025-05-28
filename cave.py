e2377933738278e207777110711045534e0d46fb
cloconut <campbellc@macstudent.com.au>
2025-05-28 23:46:56 +0000

    reordered methods + character getter and setter

--- cave.py
+++ cave.py
@@ -4,21 +4,34 @@ class Cave:
         self.name = cave_name
         self.description = None
         self.linked_caves = {}
-    # Get the cave description
-    def get_description(self):
-        return self.description
+
     # Set the cave description
     def set_description(self, cave_description):
         self.description = cave_description
-    # Display the cave description
-    def describe(self):
-        print(self.description)
-    # Get the cave name
-    def get_name(self, cave_name):
-        print(cave_name)
+
+    # Get the cave description
+    def get_description(self):
+        return self.description
+
     # Set the cave name
     def set_name(self, cave_name):
         cave_name = input('What will you name this cave?')
+
+    # Get the cave name
+    def get_name(self, cave_name):
+        print(cave_name)
+
+    # Character getting and setting ???????????
+    def set_character(self, new_character):
+        self.character = new_character
+
+    def get_character(self):
+        return self.character
+
+    # Display the cave description
+    def describe(self):
+        print(self.description)
+
     # Link cave method
     def link_cave(self, cave_to_link, direction):
         self.linked_caves[direction] = cave_to_link
