1b6750acf8b8dda28e3fae94c168a33beb817874
cloconut <campbellc@macstudent.com.au>
2025-05-28 02:24:23 +0000

    other caves and definitions

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from cave import Cave
 
-cavern = Cave('Cavern')
-grotto = Cave('Grotto')
-dungeon = Cave('Dungeon')
+hollow = Cave('Harrowing Hollow')
+hollow.set_description('Its so empty in here...')
 
-cavern.set_description('A damp and dirty cave.')
+cavern = Cave('Crusty Cavern')
+cavern.set_description('A strangely crusty cave. The walls are... shedding?')
+
+grotto = Cave('Grotty Grotto')
+grotto.set_description('Filthy, grimey, grotty. Untouched since 1812.')
+
+dungeon = Cave('Dusty Dungeon')
+dungeon.set_description('A mass is emerging from the dark clouds of dust...')
+
+lake = Cave('Lukewarm Lake')
+lake.set_description('A calm and tranquil lake, why dont I go for a dip?')
+
+mines = Cave('Merchants Mines')
+mines.set_description('How neat. A one stop shop for all necessities.')
+
+keep = Cave('The Krakens Keep')
+keep.set_description('Theres something in the water.')
+
+fissure = Cave('Freaky Fissure')
+fissure.set_description('Its said to be haunted in here, bah?')
+
+tunnel = Cave('Tyrannical Tunnel')
+tunnel.set_description('Its so cold...')
+
+cave = Cave('The Cave Rave')
+cave.set_description('Oh party party yah')
 
 cavern.describe()
