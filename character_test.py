67f60e5feead98e75201509efba72f029b7f7d04
cloconut <campbellc@macstudent.com.au>
2025-06-05 02:55:43 +0000

    sample character dialogue (method)

--- character_test.py
+++ character_test.py
@@ -1,4 +1,6 @@
 from character import Character
 asriel = Character("Asriel Dreemurr", "The absolute GOD of hyperdeath.")
+asriel.set_conversation("I don't need ANYONE!")
 
 asriel.describe()
+asriel.talk()
