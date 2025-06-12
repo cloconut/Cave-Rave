d15b65f189ac8dbfd7638707d62ebcc0d2362353
cloconut <campbellc@macstudent.com.au>
2025-06-12 22:50:12 +0000

    successful item test

--- character_test.py
+++ character_test.py
@@ -1,13 +1,18 @@
 from character import Character
 from character import Enemy
+from item import Item
 
-asriel = Enemy("Asriel Dreemurr", "The absolute GOD of hyperdeath.")
-asriel.set_conversation("I don't need ANYONE!")
-asriel.set_weakness("Mercy")
+# asriel = Enemy("Asriel Dreemurr", "The absolute GOD of hyperdeath.")
+# asriel.set_conversation("I don't need ANYONE!")
+# asriel.set_weakness("Mercy")
 
-asriel.describe()
-asriel.talk()
+# asriel.describe()
+# asriel.talk()
 
-print("What will you fight with?")
-combat_weapon = input()
-asriel.fight(combat_weapon)
+# print("What will you fight with?")
+# combat_weapon = input()
+# asriel.fight(combat_weapon)
+
+realknife = Item("Real Knife", "Offensive counterpart to the LOCKET")
+
+realknife.describe()
